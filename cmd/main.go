3c77cc77e3aece18792112c2c00b847880f0f659
Radjiv333 <dattatreya.radjiv@gmail.com>
2026-04-08 17:54:00 +0500

    feat: --help flag

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 
 var PORT int = 8080
 var SERVER string = "127.0.0.1"
+var USAGEMSG string = "$ ./1337b04rd --help\nhacker board\n\nUsage:\n\t1337b04rd [--port <N>]\n\t1337b04rd --help\n\nOptions:\n\t--help       Show this screen.\n\t--port N     Port number."
 
 func Base(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hello world")
@@ -18,12 +19,17 @@ func Base(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// Flags
 	flagSet := flag.NewFlagSet("flagSet", flag.ContinueOnError) // Using FlagSet to write custom error messages
-	flagSet.SetOutput(io.Discard) // This code makes it so that flagSet.Parse is not allowed to write its own error messages (at all)
-	port := flagSet.Int("port", PORT, "selects port on which the server will listen") 
+	flagSet.SetOutput(io.Discard)                               // This code makes it so that flagSet.Parse is not allowed to write its own error messages (at all)
+	port := flagSet.Int("port", PORT, "selects port on which the server will listen")
+	help := flagSet.Bool("help", false, "shows usage of the program")
 	err := flagSet.Parse(os.Args[1:])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
+	}
+	if *help == true {
+		fmt.Println(USAGEMSG)
+		return
 	}
 
 	// Mux
