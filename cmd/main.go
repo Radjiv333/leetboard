1cd3fa6077eb0cd26177416fe6789c9650aada70
Radjiv333 <dattatreya.radjiv@gmail.com>
2026-04-09 15:16:52 +0500

    refactor: rick and morty api

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"leetboard/internal/adapters/api"
+	"leetboard/internal/adapters/handler"
 	"net/http"
 	"os"
-	"time"
 )
 
 var PORT int = 8080
@@ -16,80 +16,33 @@ var USAGEMSG string = "$ ./1337b04rd --help\nhacker board\n\nUsage:\n\t1337b04rd
 
 func main() {
 	// Flags
-	// flagSet := flag.NewFlagSet("flagSet", flag.ContinueOnError) // Using FlagSet to write custom error messages
-	// flagSet.SetOutput(io.Discard)                               // This code makes it so that flagSet.Parse is not allowed to write its own error messages (at all)
-	// port := flagSet.Int("port", PORT, "selects port on which the server will listen")
-	// help := flagSet.Bool("help", false, "shows usage of the program")
-	// err := flagSet.Parse(os.Args[1:])
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(2)
-	// }
-	// if *help == true {
-	// 	fmt.Println(USAGEMSG)
-	// 	return
-	// }
-
-	url := "https://rickandmortyapi.com/api/character"
-	client := &http.Client{Timeout: 10 * time.Second}
-	var allCharacters []api.RickAndMortyCharacters
-	var info api.RickAndMortyInfo
-	for url != "" {
-		resp, err := client.Get(url)
-		if err != nil {
-			fmt.Println("could not get rick_and_morty api:", err)
-			os.Exit(2)
-		}
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("could not read response body:", err)
-			os.Exit(2)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			fmt.Printf("unexpected status: %s\n", resp.Status)
-			fmt.Println("body:", string(body))
-			os.Exit(2)
-		}
-
-		response := api.RickAndMortyResponse{}
-		err = json.Unmarshal(body, &response)
-		if err != nil {
-			fmt.Println("could not unmarshal body:", err)
-			os.Exit(2)
-		}
-		fmt.Println(response.Results)
-		allCharacters = append(allCharacters, response.Results...)
-		if response.Info.NextURL != nil {
-			url = *response.Info.NextURL
-		} else {
-			info = response.Info
-			url = ""
-			break
-		}
-		time.Sleep(500 * time.Millisecond)
+	flagSet := flag.NewFlagSet("flagSet", flag.ContinueOnError) // Using FlagSet to write custom error messages
+	flagSet.SetOutput(io.Discard)                               // This code makes it so that flagSet.Parse is not allowed to write its own error messages (at all)
+	port := flagSet.Int("port", PORT, "selects port on which the server will listen")
+	help := flagSet.Bool("help", false, "shows usage of the program")
+	err := flagSet.Parse(os.Args[1:])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
 	}
-	fmt.Println(info)
-	fmt.Println(allCharacters)
+	if *help == true {
+		fmt.Println(USAGEMSG)
+		return
+	}
 
-	// var characters []api.RickAndMortyCharacters = make([]api.RickAndMortyCharacters, 0)
-	// err = json.Unmarshal(body, &characters)
-	// if err != nil {
-	// 	fmt.Println("could not unmarshal body:", err)
-	// 	os.Exit(2)
-	// }
-	// fmt.Println(characters[0])
+	// Rick And Morty API
+	rickAndMortyCharacters, rickAndMortyInfo := api.FetchRickAndMortyCharacters()
+	fmt.Println(rickAndMortyCharacters)
+	fmt.Println(rickAndMortyInfo)
 
 	// Mux
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/catalog", handler.Catalog)
-	// mux.HandleFunc("/create-post", handler.CreatePost)
-	// mux.HandleFunc("/archive", handler.Archive)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/catalog", handler.Catalog)
+	mux.HandleFunc("/create-post", handler.CreatePost)
+	mux.HandleFunc("/archive", handler.Archive)
 
-	// // Server
-	// addr := fmt.Sprintf("%s:%d", SERVER, *port)
-	// fmt.Printf("Starting the server on port: %d\n", *port)
-	// http.ListenAndServe(addr, mux)
+	// Server
+	addr := fmt.Sprintf("%s:%d", SERVER, *port)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	http.ListenAndServe(addr, mux)
 }
--- internal/adapters/api/rick_and_morty.go
+++ internal/adapters/api/rick_and_morty.go
@@ -1,5 +1,14 @@
 package api
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
 type RickAndMortyResponse struct {
 	Info    RickAndMortyInfo         `json:"info"`
 	Results []RickAndMortyCharacters `json:"results"`
@@ -12,7 +21,52 @@ type RickAndMortyCharacters struct {
 }
 
 type RickAndMortyInfo struct {
-	Count   int    `json:"count"`
-	Pages   int    `json:"pages"`
+	Count   int     `json:"count"`
+	Pages   int     `json:"pages"`
 	NextURL *string `json:"next"`
+}
+
+func FetchRickAndMortyCharacters() ([]RickAndMortyCharacters, RickAndMortyInfo) {
+	url := "https://rickandmortyapi.com/api/character"
+	client := &http.Client{Timeout: 10 * time.Second} // Use http.Client instead of http.Get to configure the connection settings (like timeout)
+	var allCharacters []RickAndMortyCharacters
+	var info RickAndMortyInfo
+	for url != "" {
+		resp, err := client.Get(url)
+		if err != nil {
+			fmt.Println("could not get rick_and_morty api:", err)
+			os.Exit(2)
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("could not read response body:", err)
+			os.Exit(2)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println("unexpected status:", resp.Status)
+			fmt.Println("body:", string(body))
+			os.Exit(2)
+		}
+
+		response := RickAndMortyResponse{}
+		err = json.Unmarshal(body, &response)
+		if err != nil {
+			fmt.Println("could not unmarshal body:", err)
+			os.Exit(2)
+		}
+		fmt.Println(response.Results)
+		allCharacters = append(allCharacters, response.Results...)
+		if response.Info.NextURL != nil {
+			url = *response.Info.NextURL
+		} else {
+			info = response.Info
+			url = ""
+			break
+		}
+		time.Sleep(500 * time.Millisecond) // Introduced delay so that we dont overwhelm the 'ram' api server
+	}
+	return allCharacters, info
 }
